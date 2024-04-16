72e0c69767a1f1566a7add2c7f22e82a62b37c40
jungkweon93 <jungkweon93@gmail.com>
2024-04-17 03:01:39 +0900

    거의 완성, 테이블뷰 오류

--- WishList/RemoteProduct.swift
+++ WishList/RemoteProduct.swift
@@ -33,12 +33,7 @@ struct RemoteProduct: Codable {
     let title: String!
     let description: String!
     let price: Int!
-//    let discountPercentage: Float!
-//    let rating: Float!
-//    let stock: Int!
-//    let brand: String!
-//    let category: String!
     let thumbnail: String!
-    let images: URL!
+    let images: [String]
     
 }
--- WishList/ViewController.swift
+++ WishList/ViewController.swift
@@ -6,15 +6,67 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
+    //코어데이터에 데이터를 담기 위해 persistentContainer 선언
+    var persistentContainer: NSPersistentContainer? {
+        (UIApplication.shared.delegate as? AppDelegate)?.persistentContainer
+    }
     
+    private var currentProduct: RemoteProduct? = nil {
+        didSet {
+            //nil이 아닐 경우 가져와야되기 때문에 guard let으로 바인딩
+            guard let currentProduct = self.currentProduct else
+            { return }
+            
+            //UI 관련은 main에서 보통 관리를 함.
+            DispatchQueue.main.async {
+                self.imageView.image = nil
+                self.titleLabel.text = currentProduct.title
+                self.descriptionLabel.text = currentProduct.description
+                
+                //describing?
+                self.priceLabel.text = "\(String(describing: currentProduct.price))$"
+            }
+            
+            DispatchQueue.global().async { [weak self] in
+                if let data = try? Data(contentsOf: URL(string: currentProduct.thumbnail)!), let image = UIImage(data: data) {
+                    DispatchQueue.main.async { self?.imageView.image = image }
+                }
+            }
+        }
+    }
     
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        fetchRemoteProduct()
     }
+    
+    
+    @IBAction func tappedSkipButton(_ sender: Any) {
+        self.fetchRemoteProduct() // 새로운 상품을 불러오는 함수 호출
+    }
+    
+    @IBAction func tappedSaveProductButton(_ sender: Any) {
+        self.saveWishProduct() // 상품을 저장
+    }
+    
+    @IBAction func tappedPresentWishList(_ sender: Any) {
+        guard let nextVC = self.storyboard?.instantiateViewController(identifier: "WishListViewController") as? WishListViewController else { return }
+        
+        self.present(nextVC, animated: true)
+    }
+    
+    
+    
+    
     
     //이 함수는 데이터를 가져오는 용도로만 사용할 예정임으로 private func로
     private func fetchRemoteProduct() {
@@ -33,8 +85,9 @@ class ViewController: UIViewController {
                     print("Error: \(error)")
                 } else if let data = data {
                     do {
-                        let user = try JSONDecoder().decode(RemoteProduct.self, from: data)
-                        self.currentProduct = product //코어데이터 생성 전이라 오류
+                        let product = try JSONDecoder().decode(RemoteProduct.self, from: data)
+                        self.currentProduct = product
+                        print("Received Data: \(product)")
                     } catch {
                         print("Decode Error: \(error)")
                         
@@ -48,5 +101,20 @@ class ViewController: UIViewController {
         
     }
 
+    // currentProduct를 가져와 Core Data에 저장합니다.
+    private func saveWishProduct() {
+        guard let context = self.persistentContainer?.viewContext else { return }
+
+        guard let currentProduct = self.currentProduct else { return }
+
+        let wishProduct = Product(context: context)
+        
+        wishProduct.id = Int64(currentProduct.id)
+        wishProduct.title = currentProduct.title
+        wishProduct.price = Double(currentProduct.price)
+
+        try? context.save()
+    }
+    
 }
 
